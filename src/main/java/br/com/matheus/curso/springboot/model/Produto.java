4362996aeaa4a509ea5ae08c59ae324e227c84cf
Matheus Peixoto Matos <roanda18@gmail.com>
2025-03-12 14:20:08 -0300

    adicionado propiedades para acesso ao banco de dados postgresql e criado entity Produto.java

---
+++ src/main/java/br/com/matheus/curso/springboot/model/Produto.java
@@ -0,0 +1,44 @@
+package br.com.matheus.curso.springboot.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String nome;
+    private BigDecimal valor;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
